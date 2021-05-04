872c1a7f66b5747b492dba0f940c1af37a2b86a8
wanghaomingqaq <13904299170@163.com>
2021-05-04 21:56:07 +0800

    5.4

--- 全部喂入.py
+++ 全部喂入.py
@@ -7,56 +7,45 @@ import numpy as np
 import seaborn as sns
 sns.set(color_codes=True)
 
-df = pd.read_csv('./data/dataset03.csv')
-X_train = df.iloc[:,1:]
-df_test = pd.read_csv('./data/test_dataset.csv')
-X_test = df_test.iloc[:,1:]
-print(X_train)
-#
-# X_train = pd.read_csv('./data/dataset03.csv',usecols=['L_T1','F_PU1','S_PU1','S_PU2','F_PU2'])
-# X_test = pd.read_csv('./data/test_dataset.csv',usecols=['L_T1','F_PU1','S_PU1','S_PU2','F_PU2'])
 
-act_func = 'relu'
-# Input layer:
+df = pd.read_csv('./data/dataset03.csv')
+X_train = df.iloc[:,1:-1]
+df_verify = pd.read_csv('./data/dataset04.csv')
+X_verify = df.iloc[:,1:-1]
+df_test = pd.read_csv('./data/test_dataset.csv')
+X_test = df_test.iloc[:,1:-1]
+
 model=Sequential()
-# First hidden layer, connected to input vector X.
-model.add(Dense(64,activation=act_func,
+model.add(Dense(64,activation='relu',
                 kernel_initializer='glorot_uniform',
                 kernel_regularizer=regularizers.l2(0.0),
                 input_shape=(X_train.shape[1],)
                )
          )
-
-model.add(Dense(8,activation=act_func,
+model.add(Dense(8,activation='relu',
                 kernel_initializer='glorot_uniform'))
-
-model.add(Dense(64,activation=act_func,
+model.add(Dense(64,activation='relu',
                 kernel_initializer='glorot_uniform'))
-
 model.add(Dense(X_train.shape[1],
                 kernel_initializer='glorot_uniform'))
-
 model.compile(loss='mse',optimizer='adam')
-
-
 print(model.summary())
-
-# Train model for 100 epochs, batch size of 10:
-NUM_EPOCHS=500
-BATCH_SIZE=10
 history=model.fit(np.array(X_train),np.array(X_train),
-                  batch_size=BATCH_SIZE,
-                  epochs=NUM_EPOCHS,
-                  validation_split=0.05,
-                  verbose = 1)
+                  batch_size=10,
+                  epochs=300,
+                  validation_data=(np.array(X_verify),np.array(X_verify)),
+                  verbose=1)
 
-plt.plot(history.history['loss'],
-         'b',
-         label='Training loss')
+plt.plot(
+    history.history['loss'],
+    'b',
+    label='Training loss'
+)
 plt.legend(loc='upper right')
 plt.xlabel('Epochs')
 plt.ylabel('Loss, [mse]')
 plt.show()
+
 X_pred = model.predict(np.array(X_train))
 X_pred = pd.DataFrame(X_pred,
                       columns=X_train.columns)
@@ -69,13 +58,15 @@ sns.distplot(scored['Loss_mae'],
              bins = 10,
              kde= True,
             color = 'blue')
-plt.xlim([0.0,.5])
+plt.xlim([0.0,1.5])
 plt.show()
+
+
 X_pred = model.predict(np.array(X_test))
 X_pred = pd.DataFrame(X_pred,
                       columns=X_test.columns)
 X_pred.index = X_test.index
-threshod = 0.5
+threshod = 0.8
 scored = pd.DataFrame(index=X_test.index)
 scored['Loss_mae'] = np.mean(np.abs(X_pred-X_test), axis = 1)
 scored['Threshold'] = threshod
